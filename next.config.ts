0ba4f4f7e6327bd18e7081fe98da36091bc10226
Prakash Jha <prakashjha241@gmail.com>
2026-03-17 13:17:15 +0530

    Config Files updated

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' }
+        ]
+      }
+    ]
+  },
+  // For local development
+  serverRuntimeConfig: {
+    // Will only be available on the server side
+    WC_KEY: process.env.WC_KEY,
+    WC_SECRET: process.env.WC_SECRET
+  }
+}
 
 export default nextConfig;
